7db8c9df4f4c943c761133d01e6297cbb59dc779
Muneeb2721 <muneebnadeem1870@gmail.com>
2025-08-13 14:45:20 +0500

    Responsive checkout page

--- src/app/checkout/checkout.ts
+++ src/app/checkout/checkout.ts
@@ -2,16 +2,25 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faHeadset, faShield, faTrophy, faTruck } from '@fortawesome/free-solid-svg-icons';
+import { Frame } from '../frame/frame';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, Frame],
   templateUrl: './checkout.html',
   styleUrl: './checkout.css'
 })
+
 export class Checkout {
   faHeadset = faHeadset;
   faTruck = faTruck;
   faShield = faShield;  
   faTrophy = faTrophy;
+
+  constructor(private route:Router){}
+
+  redirectToHome(){
+    this.route.navigate(['home-page'])
+  }
 }
