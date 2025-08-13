3160b7a001b87f860933293e85538d5d090d35f2
Muneeb2721 <muneebnadeem1870@gmail.com>
2025-08-13 14:45:03 +0500

    Responsive cart page

--- src/app/cart/cart.ts
+++ src/app/cart/cart.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faHeadset, faShield, faTrash, faTrophy, faTruck } from '@fortawesome/free-solid-svg-icons';
+import { Frame } from '../frame/frame';
 
 @Component({
   selector: 'app-cart',
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, Frame],
   templateUrl: './cart.html',
   styleUrl: './cart.css'
 })
@@ -22,5 +23,9 @@ export class Cart {
 
   redirectToCheckout(){
     this.route.navigate(['checkout-page']);
+  }
+
+  redirectToHome(){
+    this.route.navigate(['home-page']);
   }
 }
